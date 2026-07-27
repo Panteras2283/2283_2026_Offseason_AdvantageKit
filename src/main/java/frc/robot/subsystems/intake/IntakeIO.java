af4a1a9f26a95eb1d4fe8a811ae0ff44eb32f231
rickgroc <ricardo_guerrero@panterasup.com>
2026-07-27 14:08:36 -0600

    Intake IO

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+/** Add your docs here. */
+public interface IntakeIO {
+    @AutoLog
+    public static class IntakeIOInputs{
+
+        public double leftPivotPos = 0.0;
+        public double rightPivotPos = 0.0;
+
+        public double feederRightVel = 0.0;
+        public double feederLeftVel = 0.0;
+        public double feederCurrent = 0.0;
+    }
+    
+    public default void updateInputs(IntakeIOInputs inputs) {}
+
+    public default void setPivotNeutralMode(NeutralModeValue mode){}
+
+    public default void setPivotPosition(double posLeft, double posRight){}
+
+    public default void setFeederVelocity(double velocity) {}
+
+    public default void setFeederDutyCycle(double percentOut) {}
+}
