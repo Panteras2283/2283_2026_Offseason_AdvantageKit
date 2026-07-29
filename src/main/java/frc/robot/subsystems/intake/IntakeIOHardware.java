01da04af791442fb13e5326bb6ddbc092b51a1d6
rickgroc <ricardo_guerrero@panterasup.com>
2026-07-29 11:52:03 -0600

    .

--- src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.feeder;
+package frc.robot.subsystems.intake;
 
 /** Add your docs here. */
-public class FeederIOHardware {}
+public class IntakeIOHardware {}
